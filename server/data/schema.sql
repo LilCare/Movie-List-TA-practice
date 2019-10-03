d3919e2b71cca783093750fb49298a6c464dbce4
Lily Carey <lilcare@Lilys-Air.hsd1.ca.comcast.net>
2019-10-02 22:03:04 -0700

    Add watched column to DB, with default false-0

--- server/data/schema.sql
+++ server/data/schema.sql
@@ -6,8 +6,9 @@ USE movieList;
 
 CREATE TABLE movieList (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(20)
+  title VARCHAR(20),
+  watched TINYINT NOT NULL DEFAULT 0
 );
 
-INSERT INTO movieList(title)
+INSERT INTO movieList(title, watched)
 VALUES ('Mean Girls'), ('Hackers'), ('The Grey'), ('Sunshine'), ('Ex Machina');
